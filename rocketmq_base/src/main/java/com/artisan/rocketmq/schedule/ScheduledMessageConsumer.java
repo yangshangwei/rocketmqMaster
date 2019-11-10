aab5ceda9f9211152658d9f65ae982a901b11939
Mr.Yang <815150141@qq.com>
2019-11-11 00:17:17 +0800

    ScheduledMessage

---
+++ rocketmq_base/src/main/java/com/artisan/rocketmq/schedule/ScheduledMessageConsumer.java
@@ -0,0 +1,45 @@
+package com.artisan.rocketmq.schedule;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * @author 小工匠
+ * @version v1.0
+ * @create 2019-11-10 17:23
+ * @motto show me the code ,change the word
+ * @blog https://artisan.blog.csdn.net/
+ * @description
+ **/
+
+public class ScheduledMessageConsumer {
+
+    public static void main(String[] args) throws Exception {
+
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("ExampleConsumer");
+        consumer.setNamesrvAddr("192.168.18.130:9876;192.168.18.131:9876");
+
+        consumer.subscribe("TestTopic", "*");
+
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> messages, ConsumeConcurrentlyContext context) {
+                for (MessageExt message : messages) {
+                    // Print approximate delay time period
+                    System.out.println("Receive message[msgId=" + message.getMsgId() + "] "
+                            + "message content is :" + new String(message.getBody()));
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        consumer.start();
+
+        //System.out.printf("Consumer Started.%n");
+    }
+}
---
+++ rocketmq_base/src/main/java/com/artisan/rocketmq/schedule/ScheduledMessageProducer.java
@@ -0,0 +1,36 @@
+package com.artisan.rocketmq.schedule;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+
+/**
+ * @author 小工匠
+ * @version v1.0
+ * @create 2019-11-10 17:23
+ * @motto show me the code ,change the word
+ * @blog https://artisan.blog.csdn.net/
+ * @description
+ **/
+
+public class ScheduledMessageProducer {
+
+    public static void main(String[] args) throws Exception {
+
+        DefaultMQProducer producer = new DefaultMQProducer("ExampleConsumer");
+
+        producer.setNamesrvAddr("192.168.18.130:9876;192.168.18.131:9876");
+
+        producer.start();
+        int totalMessagesToSend = 3;
+        for (int i = 0; i < totalMessagesToSend; i++) {
+            Message message = new Message("TestTopic", ("Hello scheduled message " + i).getBytes());
+            //延时消费
+            message.setDelayTimeLevel(6);
+            // Send the message
+            producer.send(message);
+        }
+
+        System.out.printf("message send is completed .%n");
+        producer.shutdown();
+    }
+}
